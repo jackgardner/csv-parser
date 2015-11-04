f44e0e36e8a944f3fdf1ee25c75df923e2f61b53
Jack Gardner <stonefeet@gmail.com>
2015-11-04 16:53:04 +0000

    CSV Version 1

---
+++ README.md
@@ -0,0 +1,30 @@
+# csv-parser [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Streaming CSV parser with Strategy support
+
+
+## Install
+
+```sh
+$ npm install --save csv-parser
+```
+
+
+## Usage
+
+```js
+var csvParser = require('csv-parser');
+
+csvParser('Rainbow');
+```
+
+## License
+
+MIT © [Jack Gardner]()
+
+
+[npm-image]: https://badge.fury.io/js/csv-parser.svg
+[npm-url]: https://npmjs.org/package/csv-parser
+[travis-image]: https://travis-ci.org/jackgardner/csv-parser.svg?branch=master
+[travis-url]: https://travis-ci.org/jackgardner/csv-parser
+[daviddm-image]: https://david-dm.org/jackgardner/csv-parser.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/jackgardner/csv-parser
---
+++ test/index.js
@@ -0,0 +1,77 @@
+import chai from 'chai';
+import {CSVParser} from '../lib';
+import fs from 'fs';
+import path from 'path';
+
+const read = fs.createReadStream;
+const expect = chai.expect;
+
+describe('csv-parser', function () {
+  it('simple csv', function (done) {
+    collect('dummy.csv', verify);
+    function verify(err, lines) {
+      expect(err).to.equal(false);
+      expect(lines).to.have.length(1);
+
+      done();
+    }
+  });
+
+  it('supports strings', function (done) {
+
+    const parser = new CSVParser();
+
+    parser.on('data', (data) => {
+      expect(data).to.deep.equal({ hello: 'world' });
+      done();
+    });
+
+    parser.write('hello\n');
+    parser.write('world\n');
+
+    parser.end();
+
+  });
+
+
+  it('newlines in a cell', function (done) {
+
+    collect('newlines.csv', verify);
+
+    function verify(err, lines) {
+
+      expect(err).to.equal(false);
+      expect(lines[0]).to.deep.equal({a: '1', b: '2', c: '3' });
+      expect(lines[1]).to.deep.equal({a: 'Once upon\na time', b: '5', c: '6' });
+      expect(lines[2]).to.deep.equal({a: '7', b: '8', c: '9' });
+
+      done();
+    }
+
+  });
+});
+
+function fixture(name) {
+  return path.join(__dirname, 'data', name);
+}
+
+function collect(file, opts, cb) {
+  if (typeof opts === 'function') {
+    return collect(file, {}, opts);
+  }
+  const data = read(fixture(file));
+  const lines = [];
+  const parser = new CSVParser(opts);
+  data
+    .pipe(parser)
+    .on('data', function (line) {
+      lines.push(line);
+    })
+    .on('error', function (err) {
+      cb(err, lines);
+    })
+    .on('end', function () {
+      cb(false, lines);
+    });
+  return parser;
+}
